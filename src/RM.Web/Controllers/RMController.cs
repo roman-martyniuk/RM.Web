325d22be4353355c00816856e0cacefcf20c96e0
Roman Martyniuk <roman.martyniuk@users.noreply.github.com>
2019-04-15 13:24:53 +0300

    Initial commit

---
+++ src/RM.Web/Controllers/RMController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RM.Web.Controllers
+{
+    public class RMController : Controller
+    {
+        [Route("")]
+        public IActionResult Home()
+        {
+            return View("~/Views/Home.cshtml");
+        }
+
+        [Route("projects.html")]
+        public IActionResult Projects()
+        {
+            return View("~/Views/Projects.cshtml");
+        }
+
+        [Route("profile.html")]
+        public IActionResult Profile()
+        {
+            return View("~/Views/Profile.cshtml");
+        }
+    }
+}
